0d3fffd17d2567750bdc09f0be748eadd5e0ea38
tonduy <80115135+tonduy@users.noreply.github.com>
2023-08-06 18:44:15 +0200

    Finished to exercise Part6 6.22

---
+++ part6/query-anecdotes-main/src/components/Notification.js
@@ -0,0 +1,18 @@
+const Notification = (message) => {
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  if (true) return null
+
+  return (
+    <div style={style}>
+      {message}
+    </div>
+  )
+}
+
+export default Notification
